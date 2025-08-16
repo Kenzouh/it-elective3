7abdbdf7d3ff1ab0ce8ac10e1e2f6858c933430c
Kenzo Ragundiaz（ラグンディアズ、賢三） <kenzo.ragundiaz18@gmail.com>
2025-08-16 13:56:05 +0800

    feat(lesson 3): add messy data 2 practice code

---
+++ Lesson3/Practice/l3_p7_messy_data2.r
@@ -0,0 +1,33 @@
+# Problem 1: Create a vector called fruits containing: "apple", "banana", "cherry", "date".
+
+fruits <- c("apple", "banana", "cherry", "date")
+fruits
+
+# Problem 2: Check if fruits is a vector
+
+is.vector(fruits)
+
+# Problem 3: Check the type of the fruits vector
+
+typeof(fruits)
+
+# Problem 4: How many elements are in fruits?
+
+length(fruits)
+
+# Problem 5: Add names to the fruits vector Assign the names "A", "B", "C", "D" to the elements of fruits and display the vector.
+
+names(fruits) <- c("A", "B", "C", "D")
+fruits
+
+# Problem 6: Check the structure of fruits
+
+str(fruits)
+
+# Problem 7: Create a numeric sequence from 5 to 25 using the colon operator
+
+5:25
+
+# Problem 8: Create a sequence of numbers from 3 to 15 using seq()
+
+seq(from = 3, to = 15)
